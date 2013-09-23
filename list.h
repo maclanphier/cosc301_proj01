cdcc3cd5cf77fc34ce88de9379ff02306085ff4f
cosc301 <cosc301@csvm.(none)>
2013-09-22 22:38:47 -0400

    hopefully fixed conflicts in list.h

--- list.h
+++ list.h
@@ -2,14 +2,14 @@
 #define __LIST_H__
 
 /* your list data structure declarations */
-<<<<<<< HEAD
+
 struct node{
 	int value;
 	struct node* next;
 };
-=======
+
 char **tokenify(const char *);
->>>>>>> 360fde843fc6f6dd27633688570fa97d3a29214c
+
 
 /* your function declarations associated with the list */
 void list_insert(const int, struct node**);
